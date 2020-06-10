a05ead859e8f5f170ab20720a5aca7b663736fc9
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-10 15:09:52 -0500

    test set 3

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -39,6 +39,24 @@ class FunctionTestCase(unittest.TestCase):
     def test_price_under_ten_to_thirty6(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(22, 10, .2), 18.60)
 
+    def test_price_under_thirty_to_fifty1(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(45, 5, .1), 50.83)
+
+    def test_price_under_thirty_to_fifty2(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(45, 5, .15), 48.71)
+
+    def test_price_under_thirty_to_fifty3(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(45, 5, .2), 46.59)
+
+    def test_price_under_thirty_to_fifty4(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(45, 10, .1), 46.06)
+
+    def test_price_under_thirty_to_fifty5(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(45, 10, .15), 44.20)
+
+    def test_price_under_thirty_to_fifty6(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(45, 10, .2), 42.35)
+
 
 if __name__ == '__main__':
     unittest.main()
