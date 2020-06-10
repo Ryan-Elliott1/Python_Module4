898a6e697348f1d76ef0968d6fe4a40ddd5af1af
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-10 15:00:45 -0500

    test set 2

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -21,6 +21,24 @@ class FunctionTestCase(unittest.TestCase):
     def test_price_under_ten6(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(9, 10, .2), 5.46)
 
+    def test_price_under_ten_to_thirty1(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(22, 5, .1), 24.65)
+
+    def test_price_under_ten_to_thirty2(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(22, 5, .15), 23.74)
+
+    def test_price_under_ten_to_thirty3(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(22, 5, .2), 22.84)
+
+    def test_price_under_ten_to_thirty4(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(22, 10, .1), 19.88)
+
+    def test_price_under_ten_to_thirty5(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(22, 10, .15), 19.24)
+
+    def test_price_under_ten_to_thirty6(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(22, 10, .2), 18.60)
+
 
 if __name__ == '__main__':
     unittest.main()
