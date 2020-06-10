1221b23fd79640447ffb0d4dec12c1295dc8aef6
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-10 14:54:51 -0500

    shipping cost before tax fix

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -3,12 +3,12 @@ def calculate_price(price, cash_coupon, percent_coupon):
     tax_rate = 0.06
     if price < 10:
         total = price - cash_coupon
-        total = (total - (total * percent_coupon))
-        total = (total + (total * tax_rate)) + 5.95
+        total = (total - (total * percent_coupon)) + 5.95
+        total = (total + (total * tax_rate))
         total = round(total, 2)
         return total
 
 
 if __name__ == '__main__':
-    print(calculate_price(9, 10, 0.2))
+    print(calculate_price(9, 10, .2))
 
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -4,22 +4,22 @@ from store import coupon_calculations
 
 class FunctionTestCase(unittest.TestCase):
     def test_price_under_ten1(self):
-        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 5, .1), 9.77)
+        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 5, .1), 10.12)
 
     def test_price_under_ten2(self):
-        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 5, .15), 9.55)
+        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 5, .15), 9.91)
 
     def test_price_under_ten3(self):
-        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 5, .2), 9.34)
+        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 5, .2), 9.70)
 
     def test_price_under_ten4(self):
-        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 10, .1), 5)
+        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 10, .1), 5.35)
 
     def test_price_under_ten5(self):
-        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 10, .15), 5.05)
+        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 10, .15), 5.41)
 
     def test_price_under_ten6(self):
-        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 10, .2), 5.10)
+        self.assertAlmostEqual(coupon_calculations.calculate_price(9, 10, .2), 5.46)
 
 
 if __name__ == '__main__':
