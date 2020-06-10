63fc59ee6e54a19824b42e3f44fd6aedafd87bf1
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-10 15:20:37 -0500

    Final Changes

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,3 +1,14 @@
+"""
+Program: coupon_calculations.py
+Author: Ryan Elliott
+Last date modified: 06/10/2020
+
+This program calculates the price of a transaction
+input price, cash coupon, and percent coupon
+outputs total price with discounts, shipping and tax included
+"""
+
+
 def calculate_price(price, cash_coupon, percent_coupon):
     total = 0
     tax_rate = 0.06
@@ -19,8 +30,14 @@ def calculate_price(price, cash_coupon, percent_coupon):
         total = (total + (total * tax_rate))
         total = round(total, 2)
         return total
+    elif 50 <= price:
+        total = price - cash_coupon
+        total = (total - (total * percent_coupon))
+        total = (total + (total * tax_rate))
+        total = round(total, 2)
+        return total
 
 
 if __name__ == '__main__':
-    print(calculate_price(9, 5, .1))
+    print(calculate_price(70, 5, .15))
 
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -3,6 +3,7 @@ from store import coupon_calculations
 
 
 class FunctionTestCase(unittest.TestCase):
+    # Price under $10
     def test_price_under_ten1(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(9, 5, .1), 10.12)
 
@@ -21,6 +22,7 @@ class FunctionTestCase(unittest.TestCase):
     def test_price_under_ten6(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(9, 10, .2), 5.46)
 
+    # Price from $10-$30
     def test_price_under_ten_to_thirty1(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(22, 5, .1), 24.64)
 
@@ -39,6 +41,7 @@ class FunctionTestCase(unittest.TestCase):
     def test_price_under_ten_to_thirty6(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(22, 10, .2), 18.60)
 
+    # Price from $30-$50
     def test_price_under_thirty_to_fifty1(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(45, 5, .1), 50.83)
 
@@ -57,11 +60,12 @@ class FunctionTestCase(unittest.TestCase):
     def test_price_under_thirty_to_fifty6(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(45, 10, .2), 42.35)
 
+    # Price over $50
     def test_price_under_over_fifty1(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(70, 5, .1), 62.01)
 
     def test_price_under_over_fifty2(self):
-        self.assertAlmostEqual(coupon_calculations.calculate_price(70, 5, .15), 58.57)
+        self.assertAlmostEqual(coupon_calculations.calculate_price(70, 5, .15), 58.56)
 
     def test_price_under_over_fifty3(self):
         self.assertAlmostEqual(coupon_calculations.calculate_price(70, 5, .2), 55.12)
